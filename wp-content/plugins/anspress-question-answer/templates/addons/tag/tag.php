918153ad3059bc37a32894f86e8d8cef90390585
JLNY <allenlicolumbia@gmail.com>
2021-02-20 00:23:03 -0500

    change question to prediction

--- wp-content/plugins/anspress-question-answer/templates/addons/tag/tag.php
+++ wp-content/plugins/anspress-question-answer/templates/addons/tag/tag.php
@@ -8,33 +8,33 @@
  */
 ?>
 
-<?php dynamic_sidebar( 'ap-top' ); ?>
+<?php dynamic_sidebar('ap-top');?>
 
 <div class="row">
 
-	<div id="ap-lists" class="<?php echo is_active_sidebar( 'ap-tag' ) ? 'ap-col-9' : 'ap-col-12'; ?>">
+	<div id="ap-lists" class="<?php echo is_active_sidebar('ap-tag') ? 'ap-col-9' : 'ap-col-12'; ?>">
 		<div class="ap-taxo-detail clearfix">
 
 			<h2 class="entry-title">
-				<?php echo esc_html( $question_tag->name ); ?>
-				<span class="ap-tax-item-count"><?php printf( _n( '1 Question', '%s Questions', $question_tag->count, 'anspress-question-answer' ), $question_tag->count ); ?></span>
+				<?php echo esc_html($question_tag->name); ?>
+				<span class="ap-tax-item-count"><?php printf(_n('1 Prediction', '%s Predictions', $question_tag->count, 'anspress-question-answer'), $question_tag->count);?></span>
 			</h2>
 
-			<?php if ( $question_tag->description != '' ) : ?>
+			<?php if ($question_tag->description != ''): ?>
 				<p class="ap-taxo-description"><?php echo $question_tag->description; ?></p>
-			<?php endif; ?>
+			<?php endif;?>
 
 		</div>
 
-		<?php ap_get_template_part( 'question-list' ); ?>
+		<?php ap_get_template_part('question-list');?>
 	</div>
 
-	<?php if ( is_active_sidebar( 'ap-tag' ) && is_anspress() ) : ?>
+	<?php if (is_active_sidebar('ap-tag') && is_anspress()): ?>
 
 		<div class="ap-question-right ap-col-3">
-			<?php dynamic_sidebar( 'ap-tag' ); ?>
+			<?php dynamic_sidebar('ap-tag');?>
 		</div>
 
-	<?php endif; ?>
+	<?php endif;?>
 
 </div>
